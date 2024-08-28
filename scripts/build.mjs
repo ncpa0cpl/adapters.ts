f96668fc07a082be4cacade4135a80f66581cf09
ncpa0cpl <szymonb21@gmail.com>
2024-08-28 19:01:59 +0200

    chore: add build script, update package json

---
+++ scripts/build.mjs
@@ -0,0 +1,21 @@
+import { build } from "@ncpa0cpl/nodepack";
+import path from "node:path";
+
+const ROOT_DIR = new URL("../", import.meta.url).pathname;
+const p = (...s) => path.join(ROOT_DIR, ...s);
+
+async function main() {
+  await build({
+    formats: ["esm"],
+    outDir: p("dist"),
+    srcDir: p("src"),
+    target: "ES6",
+    declarations: true,
+    tsConfig: p("tsconfig.json"),
+  });
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
